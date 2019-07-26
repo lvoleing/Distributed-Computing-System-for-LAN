fab6b1d40ec735295fb0c5c84ce31fb809668d5f
lvoleing <43130710+lvoleing@users.noreply.github.com>
2019-07-26 15:33:11 +0800

    子节点管理

--- mqNodeClass.py
+++ mqNodeClass.py
@@ -3,6 +3,9 @@ import sys
 import json
 import socket
 import logging
+import os
+import pexpect
+import subprocess
 
 class mqNodeClass:
     def __init__(self,configFilePath = "../config/config.json"):
@@ -17,10 +20,12 @@ class mqNodeClass:
         self.__mqPassword__ = self.__rabbitMQ__["password"]
         self.__mqVHost__    = self.__rabbitMQ__["virtual_host"]
 
-        self.__logger__ = logging.getLogger("GPUserver_logger")
+        self.__logger__     = logging.getLogger("GPUserver_logger")
         self.__logger__.setLevel(logging.DEBUG)
-        __handler__ = logging.FileHandler(self.__logFile__, encoding='UTF-8')
-        __logging_format = logging.Formatter(
+        if not os.path.exists(self.__logFile__):
+            os.makedirs(self.__logFile__)
+        __handler__         = logging.FileHandler(self.__logFile__, encoding='UTF-8')
+        __logging_format    = logging.Formatter(
         '%(asctime)s-%(levelname)s-%(filename)s-%(funcName)s-%(lineno)s-%(message)s')
         __handler__.setFormatter(__logging_format)
         self.__logger__.addHandler(__handler__)
@@ -29,31 +34,47 @@ class mqNodeClass:
         credentials = pika.PlainCredentials(username= self.__mqUsername__, password=self.__mqPassword__)
         self.__connection__ = pika.BlockingConnection(
             pika.ConnectionParameters(host=self.__host__,virtual_host=self.__mqVHost__, credentials=credentials))
-        self.__channel__ = self.__connection__.channel()
+        self.__channel__    = self.__connection__.channel()
         #声明路由
-        self.__channel__.exchange_declare(exchange='direct_logs', exchange_type='direct')
+        self.__channel__.exchange_declare(exchange='direct_logs', exchange_type='direct',durable=True)
 
         #声明队列
-        result = self.__channel__.queue_declare('', exclusive=True,durable=True)
+        result              = self.__channel__.queue_declare('', exclusive=True,durable=True)
         self.queue_name = result.method.queue
 
         #获得路由键
-        host = socket.gethostbyname(socket.gethostname())
-        bindingKey = host
+        host                = socket.gethostbyname(socket.gethostname())
+        bindingKey          = host
         self.__channel__.queue_bind(exchange='direct_logs', queue=self.queue_name, routing_key=bindingKey)
-        print(' [*] Waiting for logs. To exit press CTRL+C')
+        print('Waiting for logs. To exit press CTRL+C')
 
     def __queryMessage__(self):
 
-        self.__channel__.basic_consume(queue=self.queue_name, on_message_callback=self.__messageParse__, auto_ack=True)
+        self.__channel__.basic_qos(prefetch_count = 1)
+        self.__channel__.basic_consume( queue = self.queue_name,
+                                        on_message_callback = self.__messageParse__
+                                        )
         self.__channel__.start_consuming()
 
-    def __messageParse__(self,ch,method,properties,body):
-        print(" [x] %r:%r" % (method.routing_key, body))
+    def __messageParse__(self,ch,method,props,body):
+        print("%r:%r" % (method.routing_key, body))
+        body     = json.loads(body)
+        cmd      = body["cmd"]
+        # process  = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        # response = process.stdout.read().decode('gbk')
+        process  = pexpect.spawn(cmd)
+        while True:
+            response = process.readline().decode('utf-8')
+            ch.basic_publish(exchange    ='',
+                            routing_key = props.reply_to,
+                            properties  = pika.BasicProperties(correlation_id = \
+                                                            props.correlation_id),
+                            body        =response)
+            if response == '':
+                break
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
     
-
-
 if __name__ == "__main__":
     test = mqNodeClass()
     test.__queryMessage__()
