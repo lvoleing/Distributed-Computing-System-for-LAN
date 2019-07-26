58a0d6588febbba742519f77943012b68b48f5b4
lvoleing <43130710+lvoleing@users.noreply.github.com>
2019-07-26 15:32:40 +0800

    Add files via upload

--- mqManagementClass.py
+++ mqManagementClass.py
@@ -2,6 +2,8 @@ import pika
 import sys
 import json
 import logging
+import uuid
+import os
 
 class mqManagement:
     def __init__(self,configFilePath = "../config/config.json"):
@@ -22,11 +24,13 @@ class mqManagement:
         self.__mqPassword__ = self.__rabbitMQ__["password"]
         self.__mqVHost__    = self.__rabbitMQ__["virtual_host"]
         self.__nodeList__   = ["192.168.4.8","192.168.4.164","192.168.4.167"]
-
-        self.__logger__ = logging.getLogger("GPUserver_logger")
+        
+        self.__logger__     = logging.getLogger("GPUserver_logger")
         self.__logger__.setLevel(logging.DEBUG)
-        __handler__ = logging.FileHandler(self.__logFile__, encoding='UTF-8')
-        __loggingFormat__ = logging.Formatter(
+        if not os.path.exists(self.__logFile__):
+            os.makedirs(self.__logFile__)
+        __handler__         = logging.FileHandler(self.__logFile__, encoding='UTF-8')
+        __loggingFormat__   = logging.Formatter(
         '%(asctime)s-%(levelname)s-%(filename)s-%(funcName)s-%(lineno)s-%(message)s')
         __handler__.setFormatter(__loggingFormat__)
         self.__logger__.addHandler(__handler__)
@@ -37,7 +41,23 @@ class mqManagement:
             pika.ConnectionParameters(host=self.__host__,virtual_host=self.__mqVHost__, credentials=credentials))
         self.__channel__ = self.__connection__.channel()
         #声明路由
-        self.__channel__.exchange_declare(exchange='direct_logs', exchange_type='direct')
+        self.__channel__.exchange_declare(exchange='direct_logs', exchange_type='direct',durable=True)
+
+        #声明回调队列
+        result = self.__channel__.queue_declare(queue='', exclusive=True,durable=True)
+        self.callback_queue = result.method.queue
+
+        self.__channel__.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.onResponse,
+            auto_ack=True)
+    
+    def onResponse(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            body=body.decode('utf-8')
+            print(body,end='')
+            self.response = []
+            self.response.append(body)
 
     def readNodesInfo(self):
 
@@ -48,13 +68,28 @@ class mqManagement:
         if nodeIP in self.__nodeList__:
             routingKey = nodeIP
             cmd = json.dumps(cmdBody)
-            self.__channel__.basic_publish(exchange='direct_logs', routing_key=routingKey, body=cmd)
+            self.response = None
+            self.corr_id = str(uuid.uuid4())
+            self.__channel__.basic_publish( exchange='direct_logs', 
+                                            routing_key=routingKey,
+                                            properties=pika.BasicProperties(
+                                                reply_to=self.callback_queue,
+                                                correlation_id=self.corr_id,
+                                                delivery_mode=2
+                                            ),
+                                            body=cmd)
             self.__logger__.info("successfully Sent %r to %r" % (cmd,routingKey))
+
+            self.__channel__.start_consuming()
+            
+            while self.response is None:
+                self.__connection__.process_data_events()
+            ifSuccess = str(self.response)
             self.__connection__.close()
-            ifSuccess = True
-        else: 
+        else:
             ifSuccess = False
             self.__logger__.warning("%r don't exist" % (nodeIP))
+
         return ifSuccess
 
     def sentMessageToNode(self,nodeIP,infoBody):
@@ -70,6 +105,13 @@ class mqManagement:
             self.__logger__.info("%r don't exist" % (nodeIP))
             return ifSuccess
 
-
-
-
+if __name__ == "__main__":
+    test = mqManagement()
+    value     ={
+        "destination": "192.168.4.164",
+        "timestamp"  : "timestamp",
+        "user"       : "userName",
+        "cmd"        : "nvidia-smi" 
+    }
+    ip = "192.168.4.8"
+    result = test.sentCmdToNode(ip, value)
